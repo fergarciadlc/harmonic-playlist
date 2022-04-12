49d5bdd22287785fdd282dce8dd1c5794f9ab8c5
Fernando Garcia <ferchoget@gmail.com>
2022-04-11 23:05:05 -0500

    updated user class

--- src/SpotifyClient/User.py
+++ src/SpotifyClient/User.py
@@ -1,22 +1,17 @@
 from dataclasses import dataclass
-from typing import TypeVar
-
-T = TypeVar("T", bound="parent")
 
 
 @dataclass(frozen=True)
 class User:
     id: int
     display_name: str
-    email: str
     uri: str
 
     @classmethod
-    def from_api_data(cls, data: dict) -> T:
+    def from_api_data(cls, data: dict) -> "User":
         """Create user from spotify data."""
         return cls(
             id=data["id"],
             display_name=data["display_name"],
-            email=data["email"],
             uri=data["uri"],
         )
